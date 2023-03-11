abd465052eea2a9642a3b4a57e8b876c1dbcbdfc
Akehide Tecson <aketecson@gmail.com>
2023-03-11 11:25:31 +0800

    LIST VIEW FINAL

--- app/src/main/java/com/example/akeandroid/FifthActivity.java
+++ app/src/main/java/com/example/akeandroid/FifthActivity.java
@@ -2,7 +2,10 @@ package com.example.akeandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FifthActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class FifthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifth);
+        Button fifthbutton = (Button) findViewById(R.id.button5);
+        fifthbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent(FifthActivity.this, ListActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
