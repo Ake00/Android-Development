7c4caa365f50d98fcd038ba8bdd64d97514e3585
Akehide Tecson <aketecson@gmail.com>
2023-03-18 12:57:44 +0800

    ACTIVITY 1 FINISHED - TECSON

---
+++ app/src/main/java/com/example/akeandroid/DiaglogFragment.java
@@ -0,0 +1,22 @@
+package com.example.akeandroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DiaglogFragment extends androidx.fragment.app.DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_dialog, container, false);
+    }
+}
